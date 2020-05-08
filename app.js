424eed9dddf8c284548347df59c2531d07b04dbd
ojimenezl <ojimenezl@est.ups.edu.ec>
2020-05-08 14:30:39 -0500

    tercer commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const argv = require('./config/yargs').argv;
 const control = require('./controlador/control')
 const colors = require('colors');
 let comando = argv._[0];
-//oscar
+//Oscar
 switch (comando) {
     case 'publicar':
 
