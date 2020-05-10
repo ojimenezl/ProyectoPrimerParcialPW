6e0c5d28a776d49aea1ee53c56b1d0c6e0e5681f
unknown <nicolascarrasco98@hotmail.com>
2020-05-10 17:49:10 -0500

    Top cinco paises en el año especificado

--- controlador/control.js
+++ controlador/control.js
@@ -3,40 +3,40 @@ const csv = require('csv-parser'); // Encargado de parsear
 let vector = [];
 let tareaPorHAcer = [];
 const guardar = (file, country, year, out) => {
-    //let archivo = fs.readFileSync(file, "utf8");
-    //vector = archivo.toString().split(/\r?\n/);
-    //console.log(vector[5].split(',').length);
-    //console.log(vector[5].split(',')[0]);
+        //let archivo = fs.readFileSync(file, "utf8");
+        //vector = archivo.toString().split(/\r?\n/);
+        //console.log(vector[5].split(',').length);
+        //console.log(vector[5].split(',')[0]);
 
-    fs.createReadStream(file).on('error', err => {
-            console.log(err);
-        }) // Abrir archivo
-        .pipe(csv({
-            cast: true
-        }))
-        .on('data', row => {
-            for (let i = 4; i < 65; i++) {
-                if (row[i] == '' || row[i] == ' ' || row[i] == "") {
-                    row[i] = '0'
+        fs.createReadStream(file).on('error', err => {
+                console.log(err);
+            }) // Abrir archivo
+            .pipe(csv({
+                cast: true
+            }))
+            .on('data', row => {
+                for (let i = 4; i < 65; i++) {
+                    if (row[i] == '' || row[i] == ' ' || row[i] == "") {
+                        row[i] = '0'
+                    }
                 }
-            }
-            //console.log(row);
-            vector.push(row)
-        }) // Pasarlo al parseador a través de una tubería
-        .on("end", () => { // Y al finalizar, terminar lo necesario
-            //console.log(vector);
-            let data = JSON.stringify(vector);
-            fs.writeFileSync('modelo/data.json', data, (err) => {
-                    if (err) throw new Error('No se pudo guarda la data', err);
-                })
-                //console.log("Se ha terminado de leer el archivo");
-        });
+                //console.log(row);
+                vector.push(row)
+            }) // Pasarlo al parseador a través de una tubería
+            .on("end", () => { // Y al finalizar, terminar lo necesario
+                //console.log(vector);
+                let data = JSON.stringify(vector);
+                fs.writeFileSync('modelo/data.json', data, (err) => {
+                        if (err) throw new Error('No se pudo guarda la data', err);
+                    })
+                    //console.log("Se ha terminado de leer el archivo");
+            });
 
-    cargarDB()
-        //console.log(tareaPorHAcer[20]);
-    topcinco(year = 2018)
-}
-
+        cargarDB()
+            //console.log(tareaPorHAcer[20]);
+        topcinco(year = 2018)
+    }
+    //Nicolas Carrasco
 const topcinco = (year) => {
     let vec = []
     year = year % 1960 + 4;
