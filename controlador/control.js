0fe6f008dd86b0e4cf5a0d71dad4ef24c88ac656
ojimenezl <ojimenezl@est.ups.edu.ec>
2020-05-10 21:09:37 -0500

    commit cinco paises menores

--- controlador/control.js
+++ controlador/control.js
@@ -35,24 +35,82 @@ const guardar = (file, country, year, out) => {
         cargarDB()
             //console.log(tareaPorHAcer[20]);
         topcinco(year = 2018)
+        menores(country, year)
     }
     //Nicolas Carrasco
 const topcinco = (year) => {
-    let vec = []
-    year = year % 1960 + 4;
-    //console.log(year);
-    //console.log(tareaPorHAcer);
-    for (let i = 5; i < tareaPorHAcer.length; i++) {
-        let temp = {
-            codigo: tareaPorHAcer[i][1],
-            valor: parseInt(tareaPorHAcer[i][year])
+        let vec = []
+        year = year % 1960 + 4;
+        //console.log(year);
+        //console.log(tareaPorHAcer);
+        for (let i = 5; i < tareaPorHAcer.length; i++) {
+            let temp = {
+                codigo: tareaPorHAcer[i][1],
+                valor: parseInt(tareaPorHAcer[i][year])
+            }
+            vec.push(temp);
         }
-        vec.push(temp);
-    }
 
-    vec = vec.sort((a, b) => { return b.valor - a.valor });
-    console.log(vec.splice(0, 5));
-    return vec.splice(0, 5)
+        vec = vec.sort((a, b) => { return b.valor - a.valor });
+        console.log(vec.splice(0, 5));
+        return vec.splice(0, 5)
+    }
+    //Oscar Jiménez
+
+const menores = (country, year) => {
+    cargarDB()
+    var sub = 0;
+    var top = [];
+    var colum = [];
+    var pais = '';
+    var consul = `${country} - ${year}`
+    var newArray = new Array();
+    for (let i = 1; i < 65; i++) {
+        if (tareaPorHAcer[3][i] === year.toString()) {
+            for (let j = 3; j < tareaPorHAcer.length; j++) {
+                colum.push(parseInt(tareaPorHAcer[j][i]));
+                if (tareaPorHAcer[j][1] == country) {
+                    sub = tareaPorHAcer[j][i];
+                }
+            }
+        }
+    }
+    console.log(`LOS 5 PAISES CON MENORES SUSCRIPCIONES QUE ${country}`.green);
+    console.log(`SU CONSULTA:`.green);
+    console.log(`${consul} | ${sub}`.red);
+    console.log("");
+    for (var cr = 0, jm = colum.length; cr < jm; cr++) {
+        if (colum[cr]) {
+            newArray.push(colum[cr])
+        }
+
+    }
+    newArray.sort((ca, cb) => ca - cb)
+
+    console.log(`TOP | AÑO | PAISES | SUSCRIPCIONES`.green);
+    for (var jj = 1; jj < newArray.length; jj++) {
+
+        if (newArray[jj] === parseInt(sub)) {
+            for (var p = 1; p <= 5; p++) {
+                top.push(newArray[jj - p])
+            }
+            for (let i = 1; i < 65; i++) {
+                if (tareaPorHAcer[3][i] === year.toString()) {
+                    for (let j = 0; j <= top.length; j++) {
+
+                        for (var tv = 3; tv < tareaPorHAcer.length; tv++) {
+                            if (top[j] === parseInt(tareaPorHAcer[tv][i])) {
+                                console.log(j + 1, " | ", year, " | ", tareaPorHAcer[tv][1], " | ", top[j]);
+                            }
+                        }
+                    }
+                    i = 65;
+                }
+            }
+            jj = newArray.length
+        }
+
+    }
 }
 const cargarDB = () => {
     try {
@@ -65,7 +123,7 @@ const cargarDB = () => {
 
 module.exports = {
     guardar
+
 }
 
 //santiago lema
-//Oscar Jimenez
