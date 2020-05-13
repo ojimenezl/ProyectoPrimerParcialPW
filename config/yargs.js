fbe36f48a6451fed3b1ee1ac32ff975772ca462b
wjuma <52583234+wjuma@users.noreply.github.com>
2020-05-12 23:17:22 -0500

    -o campo obligatorio

--- config/yargs.js
+++ config/yargs.js
@@ -1,34 +1,35 @@
 const file = {
     demand: true,
-    alias: 'f',
-    desc: 'Path csv'
-}
+    alias: "f",
+    desc: "Path csv",
+};
 const country = {
-    alias: 'c',
-    desc: 'Codigo de pais'
-}
+    alias: "c",
+    desc: "Codigo de pais",
+};
 const year = {
-    alias: 'y',
-    default: '2018',
-    desc: 'Año'
-}
+    alias: "y",
+    default: "2018",
+    desc: "Año",
+};
 const out = {
-    alias: 'o',
-    desc: 'Path json'
-}
-const argv = require('yargs')
-    .command('publicar', 'Crear una tarea', {
-        file,
-        country,
-        year
-    }).command('guardar', 'Actualizo una tarea', {
+    demand: true,
+    alias: "o",
+    desc: "Path json",
+};
+const argv = require("yargs")
+    .command("publicar", "Crear una tarea", {
         file,
         country,
         year,
-        out
     })
-    .argv;
+    .command("guardar", "Actualizo una tarea", {
+        file,
+        country,
+        year,
+        out,
+    }).argv;
 module.exports = {
-        argv
-    }
-    //cambio
+    argv,
+};
+//cambio
